8efbc452b818ad05369adb000a5373c7edb4aa62
GlebMedvedovsky <post@medy-trans.de>
2025-03-26 23:08:24 +0100

    исправлено/corrected.

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -4,10 +4,16 @@ import { ProductProps } from "./types";
 
 function Product({ name, price }: ProductProps) {
   return (
-    <div className="product-card">
-      <h2>{name}</h2>
-      <p>Price: ${price.toFixed(2)}</p>
-    </div>
+    // <div className="product-card">
+    //   <h2>{name}</h2>
+    //   <p>Price: ${price.toFixed(2)}</p>
+    // </div>
+    // Теперь вместо формата Price: $1.50, будет отображаться просто Price: 1.50.
+
+      <div className="product-card">
+        <h2>{name}</h2>
+        <p>Price: {price.toFixed(2)}</p>
+      </div>  
   );
 }
 
--- src/homeworks/homework07/Homework07.tsx
+++ src/homeworks/homework07/Homework07.tsx
@@ -1,22 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
 import Product from "../../components/Product/Product";
 
 const products = [
-  { name: "Apple", price: 1.5 },
-  { name: "Banana", price: 1.0 },
-  { name: "Orange", price: 2.0 },
+  { id: uuidv4(), name: "Apple", price: 1.5 },
+  { id: uuidv4(), name: "Banana", price: 1.0 },
+  { id: uuidv4(), name: "Orange", price: 2.0 },
 ];
 
-const Homework07: React.FC = () => {
+function Homework07() {
+  const productCards = products.map((product) => (
+    <Product key={product.id} name={product.name} price={product.price} />
+  ));
+
   return (
     <div>
       <h1>Product List</h1>
       <div style={{ display: "flex", gap: "10px" }}>
-        {products.map((product, index) => (
-          <Product key={index} name={product.name} price={product.price} />
-        ))}
+        {productCards}
       </div>
     </div>
   );
-};
+}
 
 export default Homework07;

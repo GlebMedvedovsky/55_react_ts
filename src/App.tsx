61130612727e254137e59bdf4abc96aab899f78d
GlebMedvedovsky <post@medy-trans.de>
2025-03-30 20:06:13 +0200

    Lession09

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@
 // import Lesson06 from './lessons/Lesson06/Lesson06';
 // import Lesson07 from "./lessons/Lesson07/Lesson07"
 // import Lesson08 from "./lessons/Lesson08/Lesson08"
+import Lesson09 from "./lessons/Lesson09/Lesson09"
+
+
 
 //Homeworks imports
 // import Homework06 from "./homeworks/Homework06/Homework06"
 // import Homework07 from "./homeworks/homework07/Homework07"
 
- import Homework08 from "./homeworks/Homework08/Homework08"
+// import Homework08 from "./homeworks/Homework08/homework08"
 
 
 
@@ -20,7 +23,7 @@ function App() {
 
   return (
     <>
-      <Homework08 />
+      <Lesson09 />
     </>
   )
 }

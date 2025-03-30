dd86e345a24f25e52019d466476b688ff0b6d6bc
GlebMedvedovsky <post@medy-trans.de>
2025-03-30 22:37:38 +0200

    Homework09 с использованием @emotion/styled

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,21 @@
 // import Lesson06 from './lessons/Lesson06/Lesson06';
 // import Lesson07 from "./lessons/Lesson07/Lesson07"
 // import Lesson08 from "./lessons/Lesson08/Lesson08"
-import Lesson09 from "./lessons/Lesson09/Lesson09"
+// import Lesson09 from "./lessons/Lesson09/Lesson09"
 
 
 
 //Homeworks imports
 // import Homework06 from "./homeworks/Homework06/Homework06"
 // import Homework07 from "./homeworks/homework07/Homework07"
-
 // import Homework08 from "./homeworks/Homework08/homework08"
+import Homework09 from "./homeworks/Homework09/Homework09"; 
 
-
-
-//Consultations imports
-//  import Consultation04 from "./consultations/Consultation04/Consultation04"
 function App() {
 
   return (
     <>
-      <Lesson09 />
+      <Homework09 />
     </>
   )
 }
---
+++ src/homeworks/Homework09/styles.ts
@@ -0,0 +1,55 @@
+import styled from "@emotion/styled";
+
+// Контейнер для карточек
+export const Container = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column; /* Чтобы заголовок был сверху */
+`;
+
+// Заголовок "User Types"
+export const Title = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  color: #a52222;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+// Карточка пользователя
+export const Card = styled.div<{ vip?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  width: ${({ vip }) => (vip ? "350px" : "300px")};
+  height: ${({ vip }) => (vip ? "450px" : "400px")};
+  background-color: ${({ vip }) => (vip ? "#FFD700" : "#E0E0E0")};
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  text-align: center;
+`;
+
+// Заголовок карточки
+export const CardTitle = styled.h2`
+  font-size: 24px;
+  margin: 10px 0;
+`;
+
+// Описание карточки
+export const CardDescription = styled.p`
+  font-size: 16px;
+  color: #333;
+  margin: 10px 0;
+`;
+
+// Аватарка
+export const Avatar = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-bottom: 15px;
+`;
